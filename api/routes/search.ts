d6815771ab20b9183a970a3ce2f479210f59a867
Juan D. Jara <juanorigami@gmail.com>
2023-11-21 00:58:05 +0100

    filter valid search results

--- api/routes/search.ts
+++ api/routes/search.ts
@@ -6,14 +6,16 @@ export default async function search(req: Request, res: Response) {
   try {
     const url = `${config.searchUrl}?q=${req.query.q}`
     const { data } = await axios.get(url)
-    const json = data.map((d: any) => {
-      return {
-        id: d.videoId,
-        title: d.title,
-        lengthSeconds: d.lengthSeconds,
-        image: `https://i.ytimg.com/vi/${d.videoId}/mqdefault.jpg`
-      }
-    })
+    const json = data
+      .filter((d: any) => d.videoId)
+      .map((d: any) => {
+        return {
+          id: d.videoId,
+          title: d.title,
+          lengthSeconds: d.lengthSeconds,
+          image: `https://i.ytimg.com/vi/${d.videoId}/mqdefault.jpg`
+        }
+      })
     return res.json(json)
   } catch (err) {
     console.error(err)
